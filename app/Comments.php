af04e2ec4ed90fdfaac4daf14e3e6c19404d8f99
bodyan <bodyanua@gmail.com>
2018-09-06 13:29:46 +0300

    Created migration, model for comments

---
+++ app/Comments.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comments extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content', 'user_id'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo('App\Posts');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
